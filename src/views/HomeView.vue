5f83c4161f78af438eb7318132b14fe717932187
smart8099 <abdulbasitm810@gmail.com>
2024-02-03 07:42:51 +0000

    Remove unnecessary files and set up routing for project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
+<template>
+    <div>
+        Hello world
+
+    </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+</style>
